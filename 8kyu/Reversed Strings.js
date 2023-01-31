f0fe86bc12a91a047736df65c0e2f629ba9501c4
Gonzalo García Corrales <115544633+gongarco@users.noreply.github.com>
2023-01-31 12:33:18 +0100

    Reversed Strings

---
+++ 8kyu/Reversed Strings.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   => 'drow'
+
+// My solution:
+
+function solution(str){
+
+let reverseString = str.split('').reverse().join('');
+return reverseString;
+  
+}
